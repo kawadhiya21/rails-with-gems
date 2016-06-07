71d6a8afc3cc8b82fc19a497eaba58aabd595bc2
Kshitij Awadhiya <kawadhiya21@gmail.com>
2016-06-07 20:20:29 +0530

    Add test for admin download

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -12,12 +12,23 @@ RSpec.describe AdminController, type: :controller do
 
     user = subject.current_user
     user.update_attribute(:role, "admin")
+    @file = user.z_files.create({:file_hash => File.new(Dir.pwd + "/spec/fixtures/files/abc.pdf")})
+    @file.save(validate: false)
   end
 
   describe "GET admin_area" do
     it "should allow admin to access" do
       get :admin_area
       expect(response).to render_template("admin_area")
+    end
+  end
+
+  describe "GET show" do
+    it "should allow to download file of any user" do
+      get :show, :id => @file.id
+      # The file is not delivered because of validation issues
+      # but it allows to access it
+      expect(response.body).to eq("")    
     end
   end
 end
